aa04d8613d2f4d2dbc6551c29ffe84df06d57bd0
Kevin Chandra <kacol.bot@gmail.com>
2023-10-29 22:05:16 +0700

    transportation crud

---
+++ app/Filament/Resources/TransportationResource.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TransportationResource\Pages;
+use App\Filament\Resources\TransportationResource\RelationManagers;
+use App\Models\Transportation;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class TransportationResource extends Resource
+{
+    protected static ?string $model = Transportation::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name'),
+                Forms\Components\TextInput::make('price')
+                                          ->prefix('Rp')
+                                          ->suffix('/day')
+                                          ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('price')
+                                         ->label('Price/day')
+                                         ->prefix('Rp'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageTransportations::route('/'),
+        ];
+    }
+}
