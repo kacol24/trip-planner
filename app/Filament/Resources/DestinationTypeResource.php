684b7a37b59f3f7d55954376d63e4f3740be0e21
Kevin Chandra <kacol.bot@gmail.com>
2023-10-29 19:56:30 +0700

    destination type crud

---
+++ app/Filament/Resources/DestinationTypeResource.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DestinationTypeResource\Pages;
+use App\Filament\Resources\DestinationTypeResource\RelationManagers;
+use App\Models\DestinationType;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class DestinationTypeResource extends Resource
+{
+    protected static ?string $model = DestinationType::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageDestinationTypes::route('/'),
+        ];
+    }
+}
