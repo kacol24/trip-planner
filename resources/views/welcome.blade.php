43acb43e45fce279b8bbc56130481cdea7887a1d
Kevin Chandra <kacol.bot@gmail.com>
2023-11-02 20:33:55 +0700

    mb0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,11 @@
     <title>Itinerary</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        td p {
+            margin-bottom: 0;
+        }
+    </style>
 </head>
 <body>
 <div class="container-fluid p-0">
