f99df7647fd34e602053a4f36ce49f3858a9a05f
Kevin Chandra <kacol.bot@gmail.com>
2023-12-13 00:15:47 +0700

    deploy and deploy:clear command

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,25 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('deploy', function (){
+    $this->call('optimize');
+    $this->call('config:cache');
+    $this->call('event:cache');
+    $this->call('route:cache');
+    $this->call('view:cache');
+    $this->call('queue:restart');
+
+    $this->call('icons:cache');
+})->purpose('Cache and optimize. Used for production.');
+
+Artisan::command('deploy:clear', function () {
+    $this->call('optimize:clear');
+    $this->call('config:clear');
+    $this->call('event:clear');
+    $this->call('route:clear');
+    $this->call('view:clear');
+    $this->call('queue:restart');
+
+    $this->call('icons:clear');
+})->purpose('Clears all cache and optimizations.');
