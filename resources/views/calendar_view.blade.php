229878884bc06de5e179f570b193cf1e895a14a3
Kevin Chandra <kacol.bot@gmail.com>
2023-12-13 12:41:32 +0700

    calendar view with budget

--- resources/views/calendar_view.blade.php
+++ resources/views/calendar_view.blade.php
@@ -12,8 +12,9 @@
     <meta name="msapplication-TileColor" content="#989898">
     <meta name="theme-color" content="#989898">
 
-    <title>Trip Calendar</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Bali Trip Itinerary 23 DES 2023 - 02 JAN 2024</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         a {
             text-overflow: ellipsis;
@@ -27,7 +28,7 @@
 <body>
 <div class="container-fluid p-0">
     <div class="table-responsive">
-        <table class="table table-bordered">
+        <table class="table table-bordered w-auto">
             <thead>
             <tr>
                 <td></td>
@@ -36,7 +37,7 @@
             <tr>
                 <td></td>
                 @foreach($itineraries as $itinerary)
-                    <th class="text-center">
+                    <th class="text-center" style="min-width: 250px;max-width: 250px">
                         {{ strtoupper($itinerary->date->format('d')) }}
                     </th>
                 @endforeach
@@ -65,77 +66,83 @@
                     </td>
                 @endforeach
             </tr>
-            <tr>
-                <td></td>
-                <th class="text-center" colspan="{{ $itineraries->count() }}">
-                    BUDGET
-                </th>
-            </tr>
-            <tr class="table-light">
-                <th>
-                    Hotel
-                </th>
-                @foreach($itineraries as $itinerary)
-                    <td class="text-end text-nowrap">
-                        <table class="w-100">
-                            <tr>
-                                <td>
-                                    Rp{{ number_format($itinerary->room_rate, 0, ',', '.') }}
-                                    x {{ $itinerary->room_count }}
-                                </td>
-                                <th class="text-end">
-                                    = Rp{{ number_format($itinerary->accomodation_cost, 0, ',', '.') }}
-                                </th>
-                            </tr>
-                        </table>
-                    </td>
-                @endforeach
-            </tr>
-            <tr class="table-light">
-                <th class="text-nowrap">Transport + Fuel</th>
-                @foreach($itineraries as $itinerary)
-                    <td class="text-end text-nowrap">
-                        <table class="w-100">
-                            <tr>
-                                <td>
-                                    Rp{{ number_format($itinerary->transportation_rate, 0, ',', '.') }}<br>
-                                    + Rp{{ number_format($itinerary->fuel_cost, 0, ',', '.') }}
-                                    ({{ $itinerary->distance }}km)
-                                </td>
-                                <th class="text-end">
-                                    = Rp{{ number_format($itinerary->transportation_cost, 0, ',', '.') }}
-                                </th>
-                            </tr>
-                        </table>
-                    </td>
-                @endforeach
-            </tr>
-            <tr class="table-light">
-                <th>Meals</th>
-                @foreach($itineraries as $itinerary)
-                    <td class="text-end">
-                        Rp{{ number_format($itinerary->kuliner_cost, 0, ',', '.') }}
-                    </td>
-                @endforeach
-            </tr>
-            <tr class="table-light">
-                <th>Wisata</th>
-                @foreach($itineraries as $itinerary)
-                    <td class="text-end">
-                        Rp{{ number_format($itinerary->wisata_cost, 0, ',', '.') }}
-                    </td>
-                @endforeach
-            </tr>
-            <tr class="table-secondary">
-                <th class="text-nowrap">
-                    TOTAL: Rp{{ number_format($itineraries->sum('total_for_the_day'), 0, ',', '.') }}
-                </th>
-                @foreach($itineraries as $itinerary)
-                    <th class="text-end">
-                        Rp{{ number_format($itinerary->total_for_the_day, 0, ',', '.') }}
+            @if(request()->has('with_budget'))
+                <tr>
+                    <td></td>
+                    <th class="text-center" colspan="{{ $itineraries->count() }}">
+                        BUDGET
                     </th>
-                @endforeach
-            </tr>
+                </tr>
+                <tr class="table-light">
+                    <th>
+                        Hotel
+                    </th>
+                    @foreach($itineraries as $itinerary)
+                        <td class="text-end text-nowrap">
+                            <table class="w-100">
+                                <tr>
+                                    <td class="align-middle lh-1 w-100">
+                                        Rp{{ number_format($itinerary->room_rate, 0, ',', '.') }}
+                                        <div>
+                                            x {{ $itinerary->room_count }}
+                                        </div>
+                                    </td>
+                                    <th class="text-end">
+                                        = Rp{{ number_format($itinerary->accomodation_cost, 0, ',', '.') }}
+                                    </th>
+                                </tr>
+                            </table>
+                        </td>
+                    @endforeach
+                </tr>
+                <tr class="table-light">
+                    <th class="text-nowrap align-top">Transport + Fuel</th>
+                    @foreach($itineraries as $itinerary)
+                        <td class="text-end text-nowrap">
+                            <table class="w-100">
+                                <tr>
+                                    <td class="align-middle lh-1 w-100">
+                                        Rp{{ number_format($itinerary->transportation_rate, 0, ',', '.') }}
+                                        <div>
+                                            + Rp{{ number_format($itinerary->fuel_cost, 0, ',', '.') }}
+                                            ({{ $itinerary->distance }}km)
+                                        </div>
+                                    </td>
+                                    <th class="text-end">
+                                        = Rp{{ number_format($itinerary->transportation_cost, 0, ',', '.') }}
+                                    </th>
+                                </tr>
+                            </table>
+                        </td>
+                    @endforeach
+                </tr>
+                <tr class="table-light">
+                    <th>Meals</th>
+                    @foreach($itineraries as $itinerary)
+                        <td class="text-end">
+                            Rp{{ number_format($itinerary->kuliner_cost, 0, ',', '.') }}
+                        </td>
+                    @endforeach
+                </tr>
+                <tr class="table-light">
+                    <th>Wisata</th>
+                    @foreach($itineraries as $itinerary)
+                        <td class="text-end">
+                            Rp{{ number_format($itinerary->wisata_cost, 0, ',', '.') }}
+                        </td>
+                    @endforeach
+                </tr>
+                <tr class="table-secondary">
+                    <th class="text-nowrap">
+                        TOTAL: Rp{{ number_format($itineraries->sum('total_for_the_day'), 0, ',', '.') }}
+                    </th>
+                    @foreach($itineraries as $itinerary)
+                        <th class="text-end">
+                            Rp{{ number_format($itinerary->total_for_the_day, 0, ',', '.') }}
+                        </th>
+                    @endforeach
+                </tr>
+            @endif
             </thead>
             <tbody>
             <tr>
@@ -174,7 +181,7 @@
                                     ])
                                 >
                                     <td colspan="2"
-                                        style="height: {{ $schedule->total_cost ? 'auto' : '66px' }}"
+                                        style="height: {{ $schedule->total_cost && request()->has('with_budget') ? 'auto' : '66px' }}"
                                         @class([
                                             'border-bottom-0',
                                             'fw-bold' => $schedule->destination->destination_type_id == App\Models\DestinationType::TYPE_KULINER,
@@ -183,7 +190,7 @@
                                         {{ $schedule->destination->name }}
                                     </td>
                                 </tr>
-                                @if($schedule->total_cost)
+                                @if($schedule->total_cost && request()->has('with_budget'))
                                     <tr @class([
                                             'table-success' => $schedule->time_of_day == '10-morning',
                                             'table-info' => $schedule->time_of_day == '20-afternoon',
