9880c21a3d58018547f6fcd85388cad3ff2bebb3
Kevin Chandra <kacol.bot@gmail.com>
2025-07-03 12:33:09 +0700

    build(deps): composer update

---
+++ resources/views/nuudo.blade.php
@@ -0,0 +1,200 @@
+<div style="--swiper-theme-color: #ff9000;--swiper-navigation-sides-offset: -30px; --swiper-pagination-bottom: -30px; --swiper-navigation-top-offset: calc(50% + 30px)">
+    <div class="swiper product-swiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="row g-0">
+                    <div class="col-12 col-md-6"
+                         style="background: url('https://nuudo.id/wp-content/uploads/2021/12/home-7.jpg') no-repeat center / cover;">
+                        <img class="d-block d-md-none" src="https://nuudo.id/wp-content/uploads/2021/12/home-7.jpg"/>
+                    </div>
+                    <div class="col-12 col-md-6 bg-white">
+                        <div class="p-3">
+                            <h6 style="color: #736357;">Gentle Baby<br/>Head to Toe Wash</h6>
+                            <p style="color: #98a0a8;">Infused with plants extract of <span style="color: #e38c24;">Calendula &amp; Chamomile</span>
+                                Enriched with <span
+                                    style="color: #e38c24;">Pro Vitamin B5 &amp; Vegetable Glycerin</span>
+                                as a natural moisturizer or baby’s delicate skin</p>
+                            <div class="row">
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="https://nuudo.id/wp-content/uploads/2021/12/home-7-3.png"
+                                                                 alt=""/></div>
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="https://nuudo.id/wp-content/uploads/2021/12/home-7-2.png"
+                                                                 alt=""/></div>
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="https://nuudo.id/wp-content/uploads/2021/12/home-7-1.png"
+                                                                 alt=""/></div>
+                            </div>
+                            <a class="button button_size_2 m-0 mt-2"
+                               style="background-color: #ff9000!important; color: #ffffff;"
+                               href="https://nuudo.id/gentle-baby-head-to-toe-wash/"> <span
+                                    class="button_label">READ MORE</span> </a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="row g-0">
+                    <div class="col-12 col-md-6"
+                         style="background: url('https://nuudo.id/wp-content/uploads/2021/12/home-8.jpg') no-repeat center / cover;">
+                        <img class="d-block d-md-none" src="https://nuudo.id/wp-content/uploads/2021/12/home-8.jpg"/>
+                    </div>
+                    <div class="col-12 col-md-6 bg-white">
+                        <div class="p-3">
+                            <h6 style="color: #736357;">Bubbly Kids<br/>Body &amp; Face Wash</h6>
+                            <p style="color: #98a0a8;">Infused with plants extract of <span style="color: #e38c24;">Calendula, Chamomile &amp; Aloe Vera.</span>
+                                Enriched with <span
+                                    style="color: #e38c24;">Soybean Oil &amp; Amino Acid Proteins </span>for
+                                smooth &amp; healthy skin during kids’ growth period. Naturally scented with <span
+                                    style="color: #e38c24;">essential oil blends</span></p>
+                            <div class="row">
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="https://nuudo.id/wp-content/uploads/2021/12/home-8-1.png"
+                                                                 alt=""/></div>
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="https://nuudo.id/wp-content/uploads/2021/12/home-8-2.png"
+                                                                 alt=""/></div>
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="https://nuudo.id/wp-content/uploads/2021/12/home-8-3.png"
+                                                                 alt=""/></div>
+                            </div>
+                            <a class="button  button_size_2 m-0 mt-2"
+                               style="background-color: #ff9000!important; color: #ffffff;"
+                               href="https://nuudo.id/bubbly-kids-body-face-wash/"> <span
+                                    class="button_label">READ MORE</span> </a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="row g-0">
+                    <div class="col-12 col-md-6"
+                         style="background: url('/wp-content/uploads/2022/09/nuudo-6.jpg') no-repeat center / cover;">
+                        <img
+                            class="d-block d-md-none" src="/wp-content/uploads/2022/09/nuudo-6.jpg"/></div>
+                    <div class="col-12 col-md-6 bg-white">
+                        <div class="p-3">
+                            <h6 style="color: #736357;">Daily Moisturizing<br/>Body Lotion</h6>
+                            <p style="color: #98a0a8;">Infused with plants oil &amp; extract of <strong
+                                    style="color: #e38c24;">Calendula</strong>, <strong
+                                    style="color: #e38c24;">Sunflower</strong> &amp; <strong
+                                    style="color: #e38c24;">Soybean</strong>, Enriched with <strong
+                                    style="color: #e38c24;">Oatmilk</strong>
+                                to hydrate baby &amp; kid delicate skin. Naturally scented with <strong
+                                    style="color: #e38c24;">essential oil blends</strong></p>
+                            <div class="row">
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="/wp-content/uploads/2022/09/all-skinpng-1-2.png"
+                                                                 alt=""/></div>
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="/wp-content/uploads/2022/09/babykids.png" alt=""/>
+                                </div>
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="/wp-content/uploads/2022/09/dermatest-1.png"
+                                                                 alt=""/>
+                                </div>
+                            </div>
+                            <a class="button  button_size_2 m-0 mt-2"
+                               style="background-color: #ff9000!important; color: #ffffff;"
+                               href="/daily-moisturizing-body-lotion/"> <span class="button_label">READ MORE</span> </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="row g-0">
+                    <div class="col-12 col-md-6"
+                         style="background: url('/wp-content/uploads/2022/09/nuudo-2-2.jpg') no-repeat center / cover;">
+                        <img
+                            class="d-block d-md-none" src="/wp-content/uploads/2022/09/nuudo-2-2.jpg"/></div>
+                    <div class="col-12 col-md-6 bg-white">
+                        <div class="p-3">
+                            <h6 style="color: #736357;">Rash Cream</h6>
+                            <p style="color: #98a0a8;">Infused with <span
+                                    style="color: #e38c24;">blends of plant oils</span><br/>Enriched with <span
+                                    style="color: #e38c24;">Calendula</span>, <span
+                                    style="color: #e38c24;">squalane</span>
+                                &amp; <span style="color: #e38c24;">oatmilk</span> <img class="img-fluid w-100"
+                                                                                        src="/wp-content/uploads/2022/09/repair-help-soothe.png"
+                                                                                        alt=""/></p>
+                            <div class="row">
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="/wp-content/uploads/2022/09/newborn.png" alt=""/>
+                                </div>
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="/wp-content/uploads/2022/09/non-perfumed.png"
+                                                                 alt=""/>
+                                </div>
+                            </div>
+                            <a class="button  button_size_2 m-0 mt-2"
+                               style="background-color: #ff9000!important; color: #ffffff;" href="/rash-cream/"> <span
+                                    class="button_label">READ MORE</span> </a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="row g-0">
+                    <div class="col-12 col-md-6"
+                         style="background: url('/wp-content/uploads/2022/09/nuudo-17.jpg') no-repeat center / cover;">
+                        <img
+                            class="d-block d-md-none" src="/wp-content/uploads/2022/09/nuudo-17.jpg"/></div>
+                    <div class="col-12 col-md-6 bg-white">
+                        <div class="p-3">
+                            <h6 style="color: #736357;">Conditioning<br/>Kids Shampoo</h6>
+                            <p style="color: #98a0a8;">Infused with plant oils of <span
+                                    style="color: #e38c24;">Argan</span>
+                                and <span style="color: #e38c24;">Coconut</span> as natural moisturizer for scalp and
+                                hair.
+                                Enriched with <span style="color: #e38c24;">Amino Acid Proteins</span> for smooth,
+                                silky,
+                                and healthy kid's haid growth.</p>
+                            <div class="row">
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="/wp-content/uploads/2021/12/home-8-1.png" alt=""/>
+                                </div>
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="/wp-content/uploads/2022/09/nosls.png" alt=""/>
+                                </div>
+                            </div>
+                            <a class="button  button_size_2 m-0 mt-2"
+                               style="background-color: #ff9000!important; color: #ffffff;"
+                               href="/conditioning-kids-shampoo/"> <span class="button_label">READ MORE</span> </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="row g-0">
+                    <div class="col-12 col-md-6"
+                         style="background: url('/wp-content/uploads/2022/09/nuudo-22-scaled.jpg') no-repeat center / cover;">
+                        <img class="d-block d-md-none" src="/wp-content/uploads/2022/09/nuudo-22-scaled.jpg"/></div>
+                    <div class="col-12 col-md-6 bg-white">
+                        <div class="p-3">
+                            <h6 style="color: #736357;">Nourishing<br/>Hair Lotion</h6>
+                            <p style="color: #98a0a8;">Infused with 7 rich blends of plant &amp; seed oils from: <span
+                                    style="color: #e38c24;">Candlenut, Coconut, Argan, Sweet Almond, Camellia seed, Sunflower seed &amp; Green tea seed</span>
+                                for smooth, silky and healthy hair growth.</p>
+                            <div class="row">
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="/wp-content/uploads/2022/09/for-all-kids.png"
+                                                                 alt=""/>
+                                </div>
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="/wp-content/uploads/2022/09/noparabens.png"
+                                                                 alt=""/>
+                                </div>
+                                <div class="col-8 col-md-4"><img class="img-fluid w-100"
+                                                                 src="/wp-content/uploads/2022/09/wet-dry.png" alt=""/>
+                                </div>
+                            </div>
+                            <a class="button  button_size_2 m-0 mt-2"
+                               style="background-color: #ff9000!important; color: #ffffff;"
+                               href="/nourishing-hair-lotion/">
+                                <span class="button_label">READ MORE</span> </a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="swiper-pagination swiper-pagination--product"></div>
+    <div class="swiper-button-prev swiper-button--product" style="background-image: none;"></div>
+    <div class="swiper-button-next swiper-button--product" style="background-image: none;"></div>
+</div>
